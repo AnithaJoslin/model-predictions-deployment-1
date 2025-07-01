252def89cf8b8bb13580c8fba197c6d914c623a6
Anitha Joslin <anita_trea@yahoo.co.in>
2025-07-01 08:55:09 +0000

    Added thepub/sub trigger and containerised preprocessing process

--- data_preprocessing/preprocess_data.py
+++ data_preprocessing/preprocess_data.py
@@ -1,12 +1,24 @@
 from google.cloud import bigquery
+from google.cloud import pubsub_v1
+import queries
 
-client = bigquery.Client()
+def publish_trigger(project_id, topic_id, publisher):
+    topic_path = publisher.topic_path(project_id, topic_id)
+    message = 'Trigger prediction job'
+    future = publisher.publish(topic_path, message.encode("utf-8"))
+    print(f"Published message ID: {future.result()}")
+    
+def preprocess_data(client):
+    df = client.query(queries.input_query).to_dataframe()
 
-query = """
-INSERT INTO `elated-effect-464110-f2.breast_cancer_dataset.input_data`
-SELECT `mean radius`, `mean area`, `mean concave points`, `mean fractal dimension`, 
-`concavity error`, `concave points error`, `worst radius`, `worst area`, insert_timestamp
-FROM `elated-effect-464110-f2.breast_cancer_dataset.raw_data`
-WHERE DATE(insert_timestamp) >= (CURRENT_DATE - 1)
-"""
-df = client.query(query).to_dataframe()
+if __name__ == '__main__':
+    
+    try: 
+        client = bigquery.Client()
+        preprocess_data(client)
+        project_id = "elated-effect-464110-f2"
+        topic_id = "data-ingestion"
+        publisher = pubsub_v1.PublisherClient()
+        publish_trigger(project_id, topic_id, publisher)
+    except Exception as e:
+        print(e)
